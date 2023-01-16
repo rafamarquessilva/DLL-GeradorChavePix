1474940795e022f550416e294a957e8629106e53
Rafael Marques <rifacrp100@hotmail.com>
2023-01-16 15:45:13 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorPix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorChavePix
+{
+    public class GeradorPix
+    {
+        public static string GetChavePix()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static List<string> GetChavesPix(int numerosChaves)
+        {
+            if (numerosChaves <= 0) return null;
+
+            List<string> chavesPix = new List<string>();
+
+            for (int i = 0; i < numerosChaves; i++)
+            {
+                chavesPix.Add(GetChavePix());
+            }
+            return chavesPix;
+        }
+
+    }
+}
